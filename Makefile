8c81ed18fbbc8b8dad691d9649acfb793d53f6f4
anastasia-anufrikova <nastyaanufrikova1996@gmail.com>
2025-12-22 22:51:29 +0300

    Добавила поле `views_count` в модель `Post`. Реализовала метод увеличения просмотров в модели `Post`. Добавила метод строкового отображения объектов. Добавила связь между моделями. Расширила `Makefile`.

--- Makefile
+++ Makefile
@@ -2,4 +2,13 @@ run:
 	uv run src/django_project/manage.py runserver
 
 lint:
-	uv run pre-commit run --all
+	python -m pre_commit run --all-files
+
+makemigrations:
+	uv run src/django_project/manage.py makemigrations
+
+migrate:
+	uv run src/django_project/manage.py migrate
+
+createsuperuser:
+	uv run src/django_project/manage.py createsuperuser
---
+++ src/django_project/blog_app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+    def increase_views_count(self):
+        self.views_count += 1
+        self.save()
+
+    def __str__(self):
+        return self.title
