f6dc2f2fe2a739cfb255d4ac1f6242234640f7ac
anastasia-anufrikova <nastyaanufrikova1996@gmail.com>
2026-01-26 22:52:42 +0300

    Работа с шаблонами и статикой

--- src/django_project/blog_app/views.py
+++ src/django_project/blog_app/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from django_project.blog_app.models import Post, Category
@@ -15,12 +14,11 @@ def index(request):
 
 def post_list(request):
     posts = Post.objects.filter(published=True)
-    response_content = '<h1>Список статей</h1> <ul>'
-    for post in posts:
-        response_content += f'<li><a href="/post/{post.slug}/">{post.title}</a> {post.created_at}</li>'
-    response_content += '</ul>'
-    response_content += '<a href = "/categories/">Назад к списку категорий</a>'
-    return HttpResponse(response_content)
+    context = {
+        "posts": posts,
+        "page_title": "Список статей",
+    }
+    return render(request, "blog_app/post_list.html", context=context)
 
 def post_detail(request, post_slug):
     post = get_object_or_404(Post, slug=post_slug)
@@ -30,24 +28,18 @@ def post_detail(request, post_slug):
     return render(request, "blog_app/post_detail.html", context=context)
 
 def categories_list(request):
-    all_categories = Category.objects.all()
-    response_content = "<h1>Список категорий</h1><ul>"
-    for category in all_categories:
-        response_content += f'<li><a href="/categories/{category.id}/">{category.title}</a></li>'
-    response_content += "</ul>"
-    return HttpResponse(response_content)
+    categories = Category.objects.all()
+    context = {
+        "categories": categories,
+        "page_title": "Список категорий"
+    }
+    return render(request, "blog_app/categories_list.html", context=context)
 
 def category_detail(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
-    posts_category = Post.objects.filter(category=category, published=True)
-    if posts_category:
-        content = f'<h1>{category.title}</h1><ul>'
-        for post in posts_category:
-            content +=  f'<li><a href="/post/{post.slug}/">{post.title}</a></li>'
-        content += '</ul>'
-    else:
-        content = f'''<h1>{category.title}</h1>
-                  <p>Статей пока нет</p>
-                  '''
-    content += '<hr><a href = "/categories/">Назад к списку категорий</a>'
-    return HttpResponse(content)
+    posts = Post.objects.filter(category=category, published=True)
+    context = {
+        "posts": posts,
+        "page_title": "Статьи категории"
+    }
+    return render(request, "blog_app/post_list.html", context=context)
