9ba0ecdb40f278b14d8d1a7ab38faa4b22e0169f
anastasia-anufrikova <nastyaanufrikova1996@gmail.com>
2026-02-04 20:55:46 +0300

    Работа с формами

--- src/django_project/blog_project/urls.py
+++ src/django_project/blog_project/urls.py
@@ -21,7 +21,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog_app.urls'))
+    path('', include('blog_app.urls')),
+    path('feedback/', include('feedback_app.urls')),
+
 ]
 
 if settings.DEBUG:
---
+++ src/django_project/feedback_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django_project.feedback_app.forms import FeedbackForm
+from django_project.feedback_app.models import Feedback
+
+
+def feedback_page(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            Feedback.objects.create(
+                **data
+            )
+
+            return redirect("blog:index")
+    else:
+        form = FeedbackForm()
+
+    return render(request, "feedback_app/feedback.html", context={"form": form})
