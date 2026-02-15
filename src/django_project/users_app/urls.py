b4d03cc81a5cada08e2c7f0c680190fae7979cef
anastasia-anufrikova <nastyaanufrikova1996@gmail.com>
2026-02-15 17:43:11 +0300

    Работа с users_app, пароль

--- src/django_project/users_app/urls.py
+++ src/django_project/users_app/urls.py
@@ -1,15 +1,20 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LogoutView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path, reverse_lazy
 from . import views
+from .views import CustomLoginView, CustomPasswordChangeView
 
-app_name = 'users'
+#app_name = 'users'
 
 urlpatterns = [
     path('profile_view/', views.ProfileView.as_view(), name='profile_view'),
     path('profile_edit/', views.ProfileUpdateView.as_view(), name='profile_edit'),
     path('register/', views.RegistrationView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='users_app/login.html'), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('password_change/', PasswordChangeView.as_view(template_name='users_app/password_change.html', success_url=reverse_lazy('users:password_change_done')), name='password_change'),
+    path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
     path('password_change_done/', PasswordChangeDoneView.as_view(template_name='users_app/password_change_done.html'), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(template_name='users_app/password_reset.html', success_url = reverse_lazy('password_reset_done')), name='password_reset'),
+    path('password_reset_done/', PasswordResetDoneView.as_view(template_name='users_app/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users_app/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', PasswordResetCompleteView.as_view(template_name='users_app/password_reset_complete.html'), name='password_reset_complete')
 ]
